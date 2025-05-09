33d02799fdfe653cf406ef3c92c568a81c060874
wilfer1225 <wilfercabrera15@gmail.com>
2025-05-09 17:34:17 -0300

    index.py

---
+++ Pureba.py
@@ -0,0 +1,5 @@
+# Esto es una prueba de código en Python
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
